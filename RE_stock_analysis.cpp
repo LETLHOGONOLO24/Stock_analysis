010f627b823f992b725ff082782e8e369617eaf4
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2025-11-16 21:28:49 +0200

    Added if statements to check for the strings and added a try statement

--- RE_stock_analysis.cpp
+++ RE_stock_analysis.cpp
@@ -16,16 +16,49 @@ struct StockRecord {
 };
 
 int main() {
-    string filename = "AAPL.csv";
-    ifstream file(filename);
+    string filename;
+    cout << "Enter output filename (e.g., my_data.csv): ";
+    cin >> filename;
 
+    ifstream file(filename); // 
     if (!file.is_open()) {
-        cerr << "Error: Could not open file!" << filename << endl;
+        cerr << "Error: Could not open file " << filename << endl;
         return 1;
     }
 
     string line;
-    if (!line, file) {
-        cerr << "File is empty"
+    if (!getline(file, line)) {
+        cerr << "Error: File appears empty or corrupt." << endl;
+        return 1;
+    }
+
+    vector<StockRecord> records;
+
+    while (getline(line, file)) {
+        if (line.empty()) continue;
+        stringstream ss;
+
+        string date, closeStr, volumeStr, open, high, low;
+
+        if (!getline(ss, date, ',')) continue;
+        if (!getline(ss, closeStr, ',')) continue;
+        if (!getline(ss, volumeStr, ',')) continue;
+        if (!getline(ss, open, ',')) continue;
+        if (!getline(ss, high, ',')) continue;
+        if (!getline(ss, low, ',')) continue;
+
+        try {
+            closeStr.erase(closeStr.remove(closeStr.begin(), closeStr.end(), '$').end());
+            volumeStr.erase(volumeStr.remove(volumeStr.begin(), volumeStr.end(), ',').end());
+
+            double closeVal = stod(closeStr);
+            long long volumeVar = stoll(volumeStr);
+
+            StockRecord rec;
+            rec.date = date;
+            rec.close = closeVar;
+            rec.volume = volumeVar;
+        }
+
     }
 }
